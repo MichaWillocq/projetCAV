ede7746497bd70304bcf49446c395007b12e6938
Tony Delattre <44232718+tonydelattre@users.noreply.github.com>
2018-10-23 14:32:31 +0200

    Ajout des Entêtes
    + Entêtes sans retour
    
    % Passage au français
    
    # Table de hachage pour accès direct aux prénoms?

--- arbreGen.c
+++ arbreGen.c
@@ -3,22 +3,47 @@
 #include <string.h>
 
 
-typedef struct _person{
-    char* name;
-    char genre;
-    struct _person* father;
-    struct _person* mother;
-} Person;
+typedef struct _personne{
+    char* prenom;
+    char sexe;
+    struct _person* pere;
+    struct _person* mere;
+} Personne;
 
 typedef _lc {
-    Person* root;
-    struct _lc* suiv;
-} List;
+    Person* racine;
+    struct _lc* suivant;
+} ListeCh;
 
 FILE* load(char* filename, FILE* f); // CHARGE EN MÉMOIRE L'ARBRE STOCKÉ DANS LE FICHIER
 FILE* save(char* filename, FILE* f); // ECRIT DANS LE FICHIER L'ARBRE STOCKÉ EN MÉMOIRE
 FILE* view(FILE* f); // AFFICHE L'ARBRE STOCKÉ EN MÉMOIRE
 void exit(); // QUITTE LE PROGRAMME
+
+new(prenom)
+
+pere(char* prenom);
+mere(char* prenom);
+parents(char* prenom);
+
+gdperes(char* prenom);
+gdmeres(char* prenom);
+gdparents(char* prenom);
+ascendants(char* prenom);
+
+enfants(char* prenom);
+petitsenfants(char* prenom);
+descendants(char* prenom);
+
+partenaires(char* prenom);
+freres(char* prenom);
+soeurs(char* prenom);
+demifreres(char* prenom);
+demisoeurs(char* prenom);
+
+oncles(char* prenom);
+tantes(char* prenom);
+cousins(char* prenom);
 
 
 int main(){
